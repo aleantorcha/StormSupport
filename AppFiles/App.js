b66430fd0da1e1f9ecd8b0410837a18eb849351a
hage042 <88248168+hage042@users.noreply.github.com>
2019-10-09 05:36:38 -1100

    Update App.js

--- AppFiles/App.js
+++ AppFiles/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>I just changed this</Text>
+      <Text>I just changed this again</Text>
     </View>
   );
 }
