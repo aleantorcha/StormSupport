cc09c0f7972e1580860a72690437d16e7c6213f9
hage042 <88248168+hage042@users.noreply.github.com>
2019-10-26 10:20:40 -0400

    Working on geolocation and pins

--- AppFiles/src/PinScreen.js
+++ AppFiles/src/PinScreen.js
@@ -1,12 +1,18 @@
 import React, {Component} from 'react';
-import { Platform, StyleSheet, Text, View, Button } from 'react-native';
+import { Platform, StyleSheet, Text, View, Image, Button, TouchableOpacity } from 'react-native';
 
 export default class PinScreen extends Component {
-  render(){
+  /*render(){
     return (
       <View >
-        <Text>Pin Screen</Text> 
+      <TouchableOpacity //style = {styles.addButton} 
+          onPress={()=>this.props.navigation.navigate("CameraScreen")}>
+          <Image
+          style={{width: 50, height: 50}}
+          //source={require('asset:/shelter pin.png')}
+          />
+        </TouchableOpacity>/*
       </View>
     );
-  }
+  }*/
 }
