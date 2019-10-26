d89ddaed6171017365d8c219c0d8069ce1b8032a
hage042 <88248168+hage042@users.noreply.github.com>
2019-10-26 10:39:05 -0400

    added png files

--- AppFiles/src/PinScreen.js
+++ AppFiles/src/PinScreen.js
@@ -2,17 +2,17 @@ import React, {Component} from 'react';
 import { Platform, StyleSheet, Text, View, Image, Button, TouchableOpacity } from 'react-native';
 
 export default class PinScreen extends Component {
-  /*render(){
+  render(){
     return (
       <View >
       <TouchableOpacity //style = {styles.addButton} 
           onPress={()=>this.props.navigation.navigate("CameraScreen")}>
           <Image
           style={{width: 50, height: 50}}
-          //source={require('asset:/shelter pin.png')}
+          source={require('./assets/shelter pin.png')}
           />
         </TouchableOpacity>/*
       </View>
     );
-  }*/
+  }
 }
