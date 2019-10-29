62dba2e49ed1e015591f26f09d5b0bf8ef9fe141
Alejandro Antorcha <11467297+aleantorcha@users.noreply.github.com>
2019-10-29 00:32:14 -0400

    Finished pin page.

--- AppFiles/src/PinScreen.js
+++ AppFiles/src/PinScreen.js
@@ -54,7 +54,7 @@ const styles = StyleSheet.create({
     // width: 100,
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'flex-start',
+    justifyContent: 'center',
     alignItems: 'center',
     paddingVertical: 10,
 
