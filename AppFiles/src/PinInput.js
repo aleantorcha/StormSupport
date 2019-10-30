f7770b2c06f3ddf5a7ed7d469622f99d989a0936
Alejandro Antorcha <11467297+aleantorcha@users.noreply.github.com>
2019-10-30 00:23:36 -0400

    Some Changes: pininput, splash, and icon

--- AppFiles/src/PinInput.js
+++ AppFiles/src/PinInput.js
@@ -1,14 +1,31 @@
 import React, {Component} from 'react';
-import { Platform, StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { Platform, StyleSheet, Text, View, TextInput } from 'react-native';
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
-export default class Camera extends Component {
-  render(){
+export default class PizzaTranslator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {text:''};
+  }
+
+  render() {
     return (
       <View>
-        <Text>Title:</Text> 
-        <Text>Description:</Text> 
-        <TouchableOpacity 
+        <Text style = {styles.TitleText}>Title:</Text> 
+        <TextInput
+          style={{height: 100, boarderColor:'gray', borderWidth: 1,}}
+          placeholder="Write here..."
+          onChangeText={(text1) => this.setState({text1})}
+          value={this.state.text1}
+        />
+        <Text style = {styles.TitleText}>Description:</Text> 
+        <TextInput
+          style={{height: 100, boarderColor:'gray', boarderWidth: 1,}}
+          placeholder="Write here..."
+          onChangeText={(text2) => this.setState({text2})}
+          value={this.state.text2}
+        />
+        <TouchableOpacity style = {style.Button}
         onPress={()=>this.props.navigation.navigate("CameraScreen")}>
           <Text>Next</Text>
         </TouchableOpacity>
@@ -18,13 +35,17 @@ export default class Camera extends Component {
 }
 const styles = StyleSheet.create({
   TitleText: {
-    fontSize: 30,    
+    fontSize: 30,
+    padding: 10,    
+    color: 'gray',
   },
-  button: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingVertical: 10,
+  Button: {
+    position: 'absolute',
+    width:50,height:50,
+    backgroundColor: '#2f83ac',
+    borderRadius:50,
+    bottom:30, right:30,
+    alignItems:'center',
+    justifyContent:'center'
   }
 });
