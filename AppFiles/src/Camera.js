9b36d679391fff159d489c792db0c5984fb87810
hage042 <88248168+hage042@users.noreply.github.com>
2019-10-30 22:38:53 -0400

    i'm writing this so Ale laughs and has some happiness in this dark, sad world. i'm running out of characters soooooo

--- AppFiles/src/Camera.js
+++ AppFiles/src/Camera.js
@@ -3,7 +3,7 @@ import { Text, View, TouchableOpacity } from 'react-native';
 import * as Permissions from 'expo-permissions';
 import { Camera } from 'expo-camera';
 
-export default class Camera extends Component {
+export default class CameraExample extends React.Component {
   state = {
     hasCameraPermission: null,
     type: Camera.Constants.Type.back,
